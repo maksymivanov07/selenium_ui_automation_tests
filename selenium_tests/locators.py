c8a866682b02a589917defa7220f47fb9cfc1170
Maksym Ivanov <maxim.ivanov.89@gmail.com>
2022-11-14 19:49:02 +0200

    Homework

--- selenium_tests/locators.py
+++ selenium_tests/locators.py
@@ -2,36 +2,36 @@
 https://authenticationtest.com/
 XPATH:
 
-1. //*
-2. //@class
+1. //body/..
+2. //meta[@name='viewport']
 3. //title
 4. //a[@class="navbar-brand"]//following-sibling::span
 5. //*[contains(@class,'navbar-nav')]
-6. //title | //body
+6. //title | //body..
 7. //div[@id="navbarSupportedContent"]
 8. //a[@href='https://authenticationtest.com/statusCode/?code=200']
-9. //div[@class='collapse navbar-collapse']//a
-10. //div[@class='collapse navbar-collapse']/ul/li/a
-11.//*[@class='col-sm-6']
+9. //div[@class='collapse navbar-collapse']
+10. //footer/child::hr
+11. //*[@class='col-sm-6']/../..
 12. //*[@class='col-sm-6']/*/div/div
 13. //*[@class='col-sm-6']//div/div/div
-14. //div[@class="card-body"]/
+14. //div[@class="card-body"]//em
 15. //a[@class='navbar-brand']/..
 16. //link[@rel='stylesheet']
 17. //link[@rel='icon']
 18. //script[@integrity='sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=']
-19. //h5[@class='text-center']
+19. //a[@href='https://authenticationtest.com/HTTPAuth/']
 20. //div[@class='card-body']/ancestor::div[@class='container']
-21. //div[@class='card-body']/ancestor::div[@class='container']
-22. //button[@class='navbar-toggler']/child::span
-23. //button[@class='navbar-toggler']/descedant::
+21. //button[@class='navbar-toggler']/child::span
+22. //div[5]
+23. //div//text()[.='Authentication Test']
 24. //button[@class='navbar-toggler']/descendant::*
 25. //script[@integrity='sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=']/following::*
 26. //ul[@class]/parent::div
-27. //div//text()[.='Authentication Test']
-28. //div[last()-1]
-29. //div[5]
-30. /html/body/div/../..
+27. /html/body/div/../..
+28. //a[@href='https://authenticationtest.com/coverage/?id=1']
+29. //img[@width='30']
+30. //img[@width='30']//..
 
 https://kyiv.yasno.com.ua/schedule-turn-off-electricity
 CSS:
@@ -39,15 +39,15 @@ CSS:
 1. div.content
 2. div#app
 3. .main-menu
-4. div:first-child
+4. div[class="content home"]
 5. div:first-child header
 6. div > p
 7. [id='app']
-8. div[class="separator"]
+8. apple-pay-close
 9. div[class~="header-popup"]
 10. div[class~='header-bottom']
-11. div[class|='header']
-12. div[class$='item']
+11. div[class|='header-top']
+12. div[class$='list']
 13. c-wiz[jsrenderer$='e']
 14. div[class^='content ho']
 15. style:first-child
